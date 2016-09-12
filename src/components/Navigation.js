2a6b57a60e70338427749d4cbf0c9c76fb744a0d
kittin03 <katarinam@centrum.cz>
2016-09-13 00:04:01 +0200

    navigation - rebass test

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
+import { Button, Badge, Space } from 'rebass'
 
 class Navigation extends Component {
   render() {
     return(
       <nav>
-        <Link to="/">Photos</Link>
-        <Link to="/counter">Counter</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
+        <Link to="/"><Button href>Photos</Button></Link><Space x="1" />
+        <Link to="/counter"><Button href>Counter</Button></Link><Space x="1" />
+        <Link to="/about"><Button href>About</Button></Link><Space x="1" />
+        <Link to="/contact"><Button href>Contact</Button></Link><Space x="1" />
       </nav>
     )
   }
