16a50c5d853c1bc6f0902fdc01e7404284275db5
kittin03 <katarinam@centrum.cz>
2016-09-27 23:06:42 +0200

    small cleanup - remove not used imports

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
-import { Button, Badge, Space } from 'rebass'
+import { Button, Space } from 'rebass'
 
 class Navigation extends Component {
   render() {
